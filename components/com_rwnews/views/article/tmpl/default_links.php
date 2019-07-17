7f9dbda7c29dcc98af0188ecafbdedd54abbf78d
asharikov <xakepok@xakepok.com>
2019-07-17 14:20:44 +0300

    Target _blank

--- components/com_rwnews/views/article/tmpl/default_links.php
+++ components/com_rwnews/views/article/tmpl/default_links.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die;
 <ul class="nav justify-content-end">
     <?php foreach ($this->item->links as $link) :?>
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo $link['url'];?>"><?php echo $link['text'];?></a>
+            <a class="nav-link" href="<?php echo $link['url'];?>" target="_blank"><?php echo $link['text'];?></a>
         </li>
     <?php endforeach; ?>
 </ul>
